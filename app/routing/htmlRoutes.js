d97619f33d059bce0becbc5b71d196b42565ce7d
Jeremiah Stafford <jstafford@slmdevice.com>
2019-11-05 18:27:50 -0500

    fixed html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,6 +10,14 @@ const path = require('path');
  * @module app
  */
 module.exports = function (app) {
+  app.get("/home", function (req, res) {
+
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 
   app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
